529523ae31ad45284892ddc168a8d1837d08b1c8
shinto2024 <154789190+shinto2024@users.noreply.github.com>
2024-03-25 16:51:09 +0900

    名前も入力できるように変更

--- index.js
+++ index.js
@@ -22,9 +22,10 @@ const server = http
           })
           .on('end', () => {
             const decoded = decodeURIComponent(rawData);
+            const key = new URLSearchParams(decoded);
             console.info(`[${now}] 投稿: ${decoded}`);
             res.write(
-              `<!DOCTYPE html><html lang="ja"><body><h1>${decoded}が投稿されました</h1></body></html>`
+              `<!DOCTYPE html><html lang="ja"><body><h1>${key.get('name')}が${key.get('yaki-tofu')}に投票しました</h1></body></html>`
             );
             res.end();
           });
